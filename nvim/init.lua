b690a0001eb270aefcdd85668e2631764e91a70d
espdesign <evanpendergraft@gmail.com>
2025-09-08 11:49:43 -0400

    remove nixd

--- nvim/init.lua
+++ nvim/init.lua
@@ -588,7 +588,6 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
-        nixd = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --

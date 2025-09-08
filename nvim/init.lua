7d1e7e6747be9c3e7cbb4e712449970618ad998e
espdesign <evanpendergraft@gmail.com>
2025-09-08 11:33:55 -0400

    add nix_language server nil_ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -588,6 +588,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        nil_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -808,7 +809,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'retrobox'
     end,
   },
 
